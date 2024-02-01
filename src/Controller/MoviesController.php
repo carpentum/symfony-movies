3b07fddcabce389e0dbecb8aed306923dffe9105
Ramón <ramon.mosquera@gmail.com>
2024-02-01 12:43:02 +0100

    Repository (14) & How to compile assets (15)

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,20 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+#[ORM\Entity(repositoryClass: MovieRepository::class)]
 class MoviesController extends AbstractController
 {
-    #[Route('/movies/', name: 'app_movies',)]
+
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    #[Route('/movies', name: 'movies',)]
     public function index(): Response
     {
-        $movies = ["Avengers: Endgame", "Inception", "Loki", "Black Widow"];
+        // findAll() - SELECT * FROM movies
+        // find() - SELECT * FROM movies WHERE id = 8
+        // findBy() - SELECT * FROM movies ORDER BY id DESC
+        // findOneBy() - SELECT * FROM movies WHERE id = 7 AND title = 'The Dark Knight' ORDER BY id DESC
+        // count() - SELECT COUNT(*) FROM movies WHERE id = 7
 
-        return $this->render('index.html.twig', [
-            'movies' => $movies,
-        ]);
+        // $respository = $this->em->getRepository(Movie::class);
+
+        // // $movies = $respository->getClassName();
+        // $movies = $respository->count(['id' => 7]);
+
+        // dd($movies);
+
+        return $this->render('index.html.twig');
     }
 }
